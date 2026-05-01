a3f689676acb0616bdf8b00dd1c31b28bda7e843
Anvar Mohamed Aslam Sha <87097295+aslamshaw@users.noreply.github.com>
2026-05-01 21:42:52 +0530

    Disable app.run() for production environment
    Comment out the app.run() call to prevent running in production.

--- backend/run.py
+++ backend/run.py
@@ -2,5 +2,6 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# # DO NOT run app.run() in production
+# if __name__ == "__main__":
+#     app.run(debug=True)
